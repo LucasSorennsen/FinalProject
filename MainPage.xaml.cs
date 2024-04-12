8ada0319363a2b72a96ae5b28b334c70468dbf4e
Lucas Sorensen <126133798+LucasSorennsen@users.noreply.github.com>
2024-04-12 01:35:59 -0600

    Create MainPage.xaml.cs

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+namespace Final;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Navigation.PushAsync(new MainPage());
+    }
+
+    private void OnConfirm(object sender, EventArgs e)
+    {
+		if (UserName.Text == "UserName" && Password.Text == "Password")
+		{
+			Navigation.PushAsync(new NewPage1());
+		}
+    }
+}
+
