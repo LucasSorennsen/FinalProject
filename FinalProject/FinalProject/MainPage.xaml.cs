0b55de89a7f4d09ecbb10643b4c91f712eda07be
muhammadkhan13 <muhammad.khan13@edu.sait.ca>
2024-04-18 17:14:20 -0600

    Created a .NET MAUI Project to push to and pull from

---
+++ FinalProject/FinalProject/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalProject
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void OnConfirm(object sender, EventArgs e)
+        {
+            if (UserName.Text == "UserName" && Password.Text == "Password")
+            {
+                Navigation.PushAsync(new NewPage1());
+            }
+        }
+    }
+}
