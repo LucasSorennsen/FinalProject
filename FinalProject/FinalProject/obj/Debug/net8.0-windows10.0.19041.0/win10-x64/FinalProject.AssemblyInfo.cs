06e16a9c0f41e2e1aa20d97f6e016dbd7e32cdf4
muhammadkhan13 <muhammad.khan13@edu.sait.ca>
2024-04-18 17:32:42 -0600

    Added "Classes" Folder (first attempt at this push didn't work for some reason)

--- FinalProject/FinalProject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FinalProject.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0b55de89a7f4d09ecbb10643b4c91f712eda07be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c57c0c6b012833dd80fc5ebaa0bd3e50c15d8813")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
