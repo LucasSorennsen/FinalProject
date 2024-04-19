e63cb7e901bd646ae13dbe3cc7ff46b9572bfd09
malihaprova <151953679+malihaprova@users.noreply.github.com>
2024-04-18 22:02:27 -0600

    Update Participants.cs

--- FinalProject/FinalProject/Classes/Participants.cs
+++ FinalProject/FinalProject/Classes/Participants.cs
@@ -8,5 +8,33 @@ namespace FinalProject.Classes
 {
     internal class Participants
     {
+
+        // need participant #, name, email, phone, special needs, type (role)
+        public int parID;
+        public string parName;
+        public string parEmail;
+        public int parPhone;
+        public string parSpec;
+        public string parType;
+
+        public Participants(int ID, string name, string email, int phone, string special, string role)
+        {
+            parID = ID;
+            parName = name;
+            parEmail = email;
+            parPhone = phone;
+            parSpec = special;
+            parType = role;
+        }
+
+        public override string ToString()
+        {
+            return $"\nID:			{parID}" +
+                $"\nName:		{parName}" +
+                $"\nEmail Address:	{parEmail}" +
+                $"\nPhone Number:		{parPhone}" +
+                $"\nSpecial Needs:		{parSpec}"+
+                $"\nRole:	{parType}";
+        }
     }
 }
