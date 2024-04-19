5903cab13e375b9faadbeae2fd4bf624dbf8d809
malihaprova <151953679+malihaprova@users.noreply.github.com>
2024-04-18 22:02:04 -0600

    Update Inventory.cs

--- FinalProject/FinalProject/Classes/Inventory.cs
+++ FinalProject/FinalProject/Classes/Inventory.cs
@@ -8,5 +8,29 @@ namespace FinalProject.Classes
 {
     internal class Inventory
     {
+        // need item#, itemPrice, itemName, how many items in stock
+        public int prodID;
+        public string prodName;
+        public int prodPrice;
+        public int prodStock;
+        public int prodSold;
+
+        public Inventory (int id, string name, int price, int stock, int sold)
+        {
+           prodID = id;
+           prodName = name;
+           prodPrice = price;
+           prodStock = stock;
+           prodSold = sold;
+        }
+
+        public override string ToString()
+        {
+            return $"\nProduct ID:			{prodID}" +
+                $"\nProduct Name:		{prodName}" +
+                $"\nPrice:	{prodPrice}" +
+                $"\nCurrent Stock:		{prodStock}" +
+                $"\nTotal Sold:		{prodSold}";
+        }
     }
 }
