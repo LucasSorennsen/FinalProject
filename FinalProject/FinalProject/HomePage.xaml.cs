d63d49d9305ca0b44315b5f4c46f290e27e39a1c
malihaprova <151953679+malihaprova@users.noreply.github.com>
2024-04-20 09:25:22 -0600

    Add files via upload

---
+++ FinalProject/FinalProject/HomePage.xaml.cs
@@ -0,0 +1,23 @@
+namespace EventManagerProject;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+
+    private void ViewEvents(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ViewEvents());
+    }
+    private void CreateEventPage(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new eventCreate());
+    }
+
+    private void UpdateExisting(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new updateEvent());
+    }
+}
