a1b4684996851a5a298087ef9de0bb4250bd8e62
Lucas Sorensen <126133798+LucasSorennsen@users.noreply.github.com>
2024-04-19 19:39:47 -0600

    Add files via upload

---
+++ updateEvent.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Final;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Navigation.PushAsync(new MainPage());
+    }
+
+	 
+    private void OnConfirm(object sender, EventArgs e)
+    {
+		
+		Navigation.PushAsync(new NewPage1());
+		
+    }
+
+
+
+}
+
